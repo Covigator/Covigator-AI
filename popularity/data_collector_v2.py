7f44e4a18805c9422d51c639681a0a059bc1cf43
jaeuk520 <jaeuk520@naver.com>
2024-06-07 21:51:00 +0900

    feat: 데이터 수집 범위 확장

--- popularity/data_collector_v2.py
+++ popularity/data_collector_v2.py
@@ -5,19 +5,31 @@ import os
 from concurrent.futures import ThreadPoolExecutor
 
 
-BASE_URL = "http://openapi.seoul.go.kr:8088/44434c75716a616534354c774e7959/json/citydata_ppltn/1/5/"
+BASE_URL = "http://openapi.seoul.go.kr:8088/44434c75716a616534354c774e7959/json/citydata/1/5/"
 RETRIES = 5
 
-SEOUL_DATA_POPULATION = 'SeoulRtd.citydata_ppltn'
+CITY_DATA = 'CITYDATA'
+SEOUL_DATA_POPULATION = 'LIVE_PPLTN_STTS'
 AREA_NAME = 'AREA_NM'
 AREA_CODE = 'AREA_CD'
 AREA_CONGEST_LEVEL = 'AREA_CONGEST_LVL'
 AREA_POPULATION_MIN = 'AREA_PPLTN_MIN'
 AREA_POPULATION_MAX = 'AREA_PPLTN_MAX'
+AVERAGE_ROAD_DATA = 'AVG_ROAD_DATA'
+ROAD_TRAFFIC_STATUS = 'ROAD_TRAFFIC_STTS'
+ROAD_TRAFFIC_SPEED = 'ROAD_TRAFFIC_SPD'
+WEATHER_STATUS = 'WEATHER_STTS'
+TEMPERATURE = 'TEMP'
+HUMIDITY = 'HUMIDITY'
+PRECIPITATION = 'PRECIPITATION'
+UV_INDEX_LEVEL = 'UV_INDEX_LVL'
+PM25 = 'PM25'
+PM10 = 'PM10'
 
-def write_data_to_csv(area_name, area_code, congestion_level, population_min, population_max, file_path):
+def write_data_to_csv(area_name, area_code, congestion_level, population_min, population_max, traffic_speed, temperature, humidity,
+                           precipitation, uv_level, pm25, pm10, file_path):
 
-    fieldnames = ['지역명', '지역 코드', '혼잡도', '최소 인구수', '최대 인구수']
+    fieldnames = ['지역명', '지역코드', '혼잡도', '최소인구수', '최대인구수', '전체도로소통평균속도', '기온', '습도', '강수량', '자외선지수단계', '초미세먼지농도', '미세먼지농도']
 
     # 파일이 존재하지 않으면 새로 생성
     if not os.path.isfile(file_path):
@@ -31,10 +43,17 @@ def write_data_to_csv(area_name, area_code, congestion_level, population_min, po
 
         writer.writerow({
             '지역명': area_name,
-            '지역 코드': area_code,
+            '지역코드': area_code,
             '혼잡도': congestion_level,
-            '최소 인구수': population_min,
-            '최대 인구수': population_max
+            '최소인구수': population_min,
+            '최대인구수': population_max,
+            '전체도로소통평균속도': traffic_speed,
+            '기온': temperature,
+            '습도': humidity,
+            '강수량': precipitation,
+            '자외선지수단계': uv_level,
+            '초미세먼지농도': pm25,
+            '미세먼지농도': pm10
         })
 
 def request_data(area_code):
@@ -65,23 +84,33 @@ def do_thread_collect(i):
     data = request_data(area_code)
 
     if data:
-        area_name = data[SEOUL_DATA_POPULATION][0][AREA_NAME]
-        area_code = data[SEOUL_DATA_POPULATION][0][AREA_CODE]
-        congestion_level = data[SEOUL_DATA_POPULATION][0][AREA_CONGEST_LEVEL]
-        population_min = data[SEOUL_DATA_POPULATION][0][AREA_POPULATION_MIN]
-        population_max = data[SEOUL_DATA_POPULATION][0][AREA_POPULATION_MAX]
+        area_name = data[CITY_DATA][AREA_NAME]
+        area_code = data[CITY_DATA][AREA_CODE]
+        congestion_level = data[CITY_DATA][SEOUL_DATA_POPULATION][0][AREA_CONGEST_LEVEL]
+        population_min = data[CITY_DATA][SEOUL_DATA_POPULATION][0][AREA_POPULATION_MIN]
+        population_max = data[CITY_DATA][SEOUL_DATA_POPULATION][0][AREA_POPULATION_MAX]
+        traffic_speed = data[CITY_DATA][ROAD_TRAFFIC_STATUS][AVERAGE_ROAD_DATA][ROAD_TRAFFIC_SPEED]
+        temperature = data[CITY_DATA][WEATHER_STATUS][0][TEMPERATURE]
+        humidity = data[CITY_DATA][WEATHER_STATUS][0][HUMIDITY]
+        precipitation = data[CITY_DATA][WEATHER_STATUS][0][PRECIPITATION]
+        uv_level = data[CITY_DATA][WEATHER_STATUS][0][UV_INDEX_LEVEL]
+        pm25 = data[CITY_DATA][WEATHER_STATUS][0][PM25]
+        pm10 = data[CITY_DATA][WEATHER_STATUS][0][PM10]
+
+        if precipitation == '-': precipitation = 0
 
         data_folder = os.path.join(os.getcwd(), 'popularity', 'data')
         file_path = os.path.join(data_folder, f'{area_name}.csv')
 
-        write_data_to_csv(area_name, area_code, congestion_level, population_min, population_max, file_path)
+        write_data_to_csv(area_name, area_code, congestion_level, population_min, population_max, traffic_speed, temperature, humidity,
+                           precipitation, uv_level, pm25, pm10, file_path)
 
 def main():
 
     start_time = time.time()  # 실행 시간 측정 시작
 
     with ThreadPoolExecutor(max_workers=5) as executor:
-        executor.map(do_thread_collect, range(1, 116))
+        executor.map(do_thread_collect, range(1, 2))
 
     end_time = time.time()
     execution_time = end_time - start_time
